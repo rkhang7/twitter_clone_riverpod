0ac0252a35077f68c76744397bfccab46c8bd8f0
khang <khang220300@gmail.com>
2023-06-01 15:06:48 +0700

    like button animation

--- lib/features/tweet/widget/tweet_card.dart
+++ lib/features/tweet/widget/tweet_card.dart
@@ -1,6 +1,8 @@
 import 'package:any_link_preview/any_link_preview.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:flutter_svg/svg.dart';
+import 'package:like_button/like_button.dart';
 import 'package:twitter_clone/common/common.dart';
 import 'package:twitter_clone/constants/assets_constants.dart';
 import 'package:twitter_clone/core/enums/tweet_type_enum.dart';
@@ -97,10 +99,35 @@ class TweetCard extends ConsumerWidget {
                                   text: tweet.reshareCount.toString(),
                                   onTap: () {},
                                 ),
-                                TweetIconButton(
-                                  pathName: AssetsConstants.likeOutlinedIcon,
-                                  text: tweet.likes.length.toString(),
-                                  onTap: () {},
+                                // TweetIconButton(
+                                //   pathName: AssetsConstants.likeOutlinedIcon,
+                                //   text: tweet.likes.length.toString(),
+                                //   onTap: () {},
+                                // ),
+                                LikeButton(
+                                  size: 25,
+                                  likeBuilder: (bool isLiked) {
+                                    return isLiked
+                                        ? SvgPicture.asset(
+                                            AssetsConstants.likeFilledIcon,
+                                            color: Pallete.redColor,
+                                          )
+                                        : SvgPicture.asset(
+                                            AssetsConstants.likeOutlinedIcon,
+                                            color: Pallete.greyColor,
+                                          );
+                                  },
+                                  likeCount: tweet.likes.length,
+                                  countBuilder: (likeCount, isLiked, text) {
+                                    return Text(
+                                      text,
+                                      style: TextStyle(
+                                        color: isLiked
+                                            ? Pallete.redColor
+                                            : Pallete.whiteColor,
+                                      ),
+                                    );
+                                  },
                                 ),
                                 IconButton(
                                   onPressed: () {},
